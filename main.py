fbcd12f5529d3590909665e94b8f86f37dd0e19f
Andrew Chen <1062446749@qq.com>
2022-06-30 20:03:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ import torch
 from D2Landscape import D2Landscape
 from torchvision import transforms
 
-model = resnet18(pretrained=True)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = resnet18(pretrained=True).to(device)
 image = imread('image.jpg') / 255
 image = torch.tensor(image, dtype=torch.float32)
 image = image.permute(2, 0, 1)
 norm = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 image = norm(image)
 image = image.unsqueeze(0)
-pre = model(image)
+pre = model(image.to(device))
 _, category = torch.max(pre, dim=1)
-criterion = torch.nn.CrossEntropyLoss()
+criterion = torch.nn.CrossEntropyLoss().to(device)
 
-a = D2Landscape(lambda x: criterion(model(x), torch.tensor([category])), image)
+x = image.clone().to(device)
+x.requires_grad = True
+label = torch.tensor([category], device=device)
+optimizer = torch.optim.Adam([x], lr = 1e-3, maximize = True)
+for i in range(100):
+    pre = model(x)
+    loss = criterion(pre, label)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print(i, loss.item())
+
+a = D2Landscape(lambda x: criterion(model(x), label), x.detach())
 a.synthesize_coordinates()
 a.draw()
